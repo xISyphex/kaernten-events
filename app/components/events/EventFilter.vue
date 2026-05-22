d411bcd77c5064a34ddfa4b7a001df7af5feb11a
xISyphex <svenbluem@outlook.com>
2026-05-22 16:34:51 +0200

    Filter behavior and UI fix

--- app/components/events/EventFilter.vue
+++ app/components/events/EventFilter.vue
@@ -172,17 +172,17 @@ const themeLabel = computed(() => {
 
 const activeFilterPills = computed(() => {
   const pills: { label: string; remove: () => void }[] = []
- 
-  if (topOnly.value)
-    pills.push({ label: 'TOP-Events', remove: () => { topOnly.value = false; syncAppliedFilters() } }) 
- 
-  stagedTowns.value.forEach(t =>
-    pills.push({ label: t, remove: () => { toggleTown(t); syncAppliedFilters() } }))
-  stagedGroups.value.forEach(g =>
-    pills.push({ label: g, remove: () => { toggleGroup(g); syncAppliedFilters() } }))
-  stagedThemes.value.forEach(th =>
-    pills.push({ label: th, remove: () => { toggleTheme(th); syncAppliedFilters() } }))
- 
+
+  if (appliedTopOnly.value)
+    pills.push({ label: 'TOP-Events', remove: () => { topOnly.value = false; syncAppliedFilters() } })
+
+  appliedTowns.value.forEach(t =>
+    pills.push({ label: t, remove: () => { toggleTown(t); stagedTowns.value = stagedTowns.value.filter(x => x !== t); syncAppliedFilters() } }))
+  appliedGroups.value.forEach(g =>
+    pills.push({ label: g, remove: () => { stagedGroups.value = stagedGroups.value.filter(x => x !== g); syncAppliedFilters() } }))
+  appliedThemes.value.forEach(th =>
+    pills.push({ label: th, remove: () => { stagedThemes.value = stagedThemes.value.filter(x => x !== th); syncAppliedFilters() } }))
+
   return pills
 })
 
@@ -398,8 +398,12 @@ const activeFilterPills = computed(() => {
         <button
           v-if="isApplied"
           @click="clearFilters"
-          class="h-12 rounded-2xl border border-gray-300 bg-white px-5 text-sm font-medium hover:bg-gray-50 text-black transition"
+          class="h-12 rounded-2xl flex flex-row gap-2 items-center border border-gray-300 bg-white px-5 text-sm font-medium hover:bg-gray-50 text-black transition"
         >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="size-5">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+        </svg>
+
           Zurücksetzen
         </button>
 
